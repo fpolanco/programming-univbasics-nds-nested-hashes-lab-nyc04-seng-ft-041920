dd25007430eb2785bab9768a8ab33f0427a5b546
Franchell Polanco <polanco.franchell@gmail.com>
2020-04-28 19:58:18 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -13,13 +13,14 @@ def assembled_aoh
 end
 
 def literal_aoh
-  array = [
+  array1 = [
     [{ name:"Don Gately", occupation:"Live-in Staff" }],
       [{ name:"Joelle van Dyne", occupation:"Radio Personality" }],
        [{ name:"Pat Monteseian", occupation:"Staff" }],
          [{ name:"Kate Gompert", occupation:"None" }],
           [ { name:"Bruce Green", occupation:"Fan of Mildred" }]
         ]
+      array1
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
 end
